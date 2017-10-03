078bb78f70efa30d3cacc05d4f5a6601b5f78476
Craig <craigodawg19@gmail.com>
2017-10-02 23:42:15 -0400

    initial commit

---
+++ NumberGuessing/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NumberGuessing.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        private static int GenerateRand()
+        {
+                Random r = new Random();
+                int rand = r.Next(1, 101);  //generate from 1-100, inclusive
+                return rand;
+        }
+        
+
+        public ActionResult Index()
+        {
+            Session["rand"] = GenerateRand();
+            return View();
+        }
+        
+        [HttpPost]
+        public JsonResult Check(int toCheck)
+        {
+            int result = toCheck - (int)Session["rand"];
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ NumberGuessing/Scripts/checker.js
@@ -0,0 +1,36 @@
+﻿function check() {
+    var entered = document.getElementById("numberField").value;
+
+    $.ajax({
+        type: "POST",
+        //$('#form-container').data('request-url')
+        url: "/Home/Check",
+        data: { toCheck: entered },
+        dataType: "json",
+        success: successFunc,
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert("Error: "+xhr.status);
+        }
+    })
+}
+
+function successFunc(data) {
+    if (data==0) {
+        //winner
+        if (confirm('Congrats! You guessed the number right! Play again?')) {
+            window.location.reload();
+        } else {
+            //Bask in your glory with a congratulations message
+            document.getElementById("hints").innerHTML = "<span style='color: #000000'>Congrats, You Won!!</span>";
+        }
+
+    } else if (data>0) {
+        //too high
+        document.getElementById("numberField").value = "";
+        document.getElementById("hints").innerHTML = 'Too High!';
+    } else {
+        //too low
+        document.getElementById("numberField").value = "";
+        document.getElementById("hints").innerHTML = 'Too Low!';
+    }
+}
